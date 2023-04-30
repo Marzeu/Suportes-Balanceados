a6127509db1fa6aa1b15c28ed3749d54959e61b6
Marzeu <marzahell@hotmail.com>
2023-04-30 18:18:06 -0300

    Add project files.

---
+++ SuporteBalanceados/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SuporteBalanceados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada = @"((({}{}))()";
+            var stack = new Stack<char>();
+
+            foreach (var item in entrada)
+            {
+                if (item == '(' || item == '[' || item == '{')
+                {
+                    stack.Push(item);
+                }
+                else
+                {
+                    switch (item)
+                    {
+                        case ')':
+                            if (stack.Peek() == '(') stack.Pop();
+                            break;
+                        case ']':
+                            if (stack.Peek() == '[') stack.Pop();
+                            break;
+                        case '}':
+                            if (stack.Peek() == '{') stack.Pop();
+                            break;
+
+                        default: break;
+                    }
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(entrada + " é válido");
+            }
+            else
+            {
+                Console.WriteLine(entrada + " não é válido");
+            }
+        }
+    }
+}
